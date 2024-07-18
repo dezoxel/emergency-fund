e8bdb18abc8201373f293fd9d81087627b96281f
Dez Oxel <dezoxel@gmail.com>
2024-07-18 15:15:13 -0400

    infra: fetch_terms_history_last_year - take full history of APY changes support

--- src/infra/savings_account_apy_history_repo.py
+++ src/infra/savings_account_apy_history_repo.py
@@ -18,3 +18,25 @@ def fetch_apy_history_last_year(conn: Connection, current_date: datetime):
     df = pd.read_sql_query(query, conn, params=[one_year_ago])
     
     return df
+
+def fetch_terms_history_last_year(conn: Connection, current_date: datetime):
+    # TODO: Handle leap years
+    one_year_ago = current_date - timedelta(days=365)
+
+    query = """
+    SELECT account_id, sa.account_name, apy, STRFTIME('%Y-%m-%d', effective_date) AS date, compound_frequency as n
+    FROM savings_accounts_apy_history saah
+    LEFT JOIN savings_accounts sa ON saah.account_id = sa.id
+    WHERE effective_date >= ?
+    ORDER BY account_id, date
+    """
+
+    df = pd.read_sql_query(query, conn, params=[one_year_ago])
+
+    if df.empty:
+       print('APY history is empty in DB.')
+       return None
+
+    df['apy'] = df['apy']/100
+
+    return df
