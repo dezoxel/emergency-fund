3c8d798d68e1efdaed2386e89057524b29462266
Dez Oxel <dezoxel@gmail.com>
2024-07-19 14:48:15 -0400

    domain: Sharpe Ratio for Account: add support for Return Rate calculation

--- src/domain/account_sharpe_ratio.py
+++ src/domain/account_sharpe_ratio.py
@@ -2,7 +2,7 @@ from datetime import datetime
 import pandas as pd
 from typing import Dict, List
 
-from domain.balance import calc_returns 
+from domain.balance import calc_future_value_stats
 from domain.sharpe_ratio_math import calc_sharpe_ratio
 
 # TODO: replace with dataclass?
@@ -10,27 +10,38 @@ ShareRatioStatsByAccount = Dict[str, Dict[str, List[float]]]
 
 def calc_sr_stats_by_account(P: float, current_date: datetime, terms_history: pd.DataFrame, rfr_history: pd.DataFrame) -> pd.DataFrame:
     def calc_returns_for_account(account_terms_history: pd.DataFrame) -> float:
-        returns = calc_returns(P, current_date, account_terms_history)
-        return returns
+        keys = ('accumulated_amount', 'balance_history', 'return_rate_history', 'return_rate_total')
+        stats = calc_future_value_stats(P, current_date, account_terms_history)
+        stats_dict = dict(zip(keys, stats))
+        return stats_dict
 
-    returns_df = terms_history.groupby('account_id').apply(calc_returns_for_account).reset_index()
-    returns_df.columns = ['account_id', 'returns']
+    stats_df = terms_history.groupby('account_id').apply(calc_returns_for_account).reset_index()
+    stats_df.columns = ['account_id', 'stats']
 
     rfr_list = rfr_history['rfr'].tolist()
-    stats = {row['account_id']: {'returns': row['returns'], 'rfr': rfr_list} for _, row in returns_df.iterrows()}
+    stats = {
+        row['account_id']: {
+            'returns': row['stats']['return_rate_history'][1:],
+            'rfr': rfr_list,
+            'return_rate_total': row['stats']['return_rate_total']
+        }
+        for _, row in stats_df.iterrows()
+    }
 
     return stats
 
 def calc_sharpe_ratios_by_account(sr_stats_by_acc: ShareRatioStatsByAccount) -> pd.DataFrame:
-    results = {'account_id': [], 'sr': []}
+    results = {'account_id': [], 'sr': [], 'return_rate': []}
     
     for account_id, values in sr_stats_by_acc.items():
         returns = values['returns']
         risk_free_rates = values['rfr']
+        return_rate = values['return_rate_total']
         sharpe_ratio = calc_sharpe_ratio(returns, risk_free_rates)
         results['account_id'].append(account_id)
         results['sr'].append(sharpe_ratio)
-    
+        results['return_rate'].append(return_rate)
+
     return pd.DataFrame(results)
 
 def calc_sharpe_ratio_for_all_accounts(P, current_date, terms_history_df, rfr_history_df):
@@ -42,9 +53,9 @@ def calc_sharpe_ratio_for_all_accounts(P, current_date, terms_history_df, rfr_hi
 def calc_best_savings_account_by_sharpe_ratio(P, current_date, terms_history_df, rfr_history_df):
     sr_df = calc_sharpe_ratio_for_all_accounts(P, current_date, terms_history_df, rfr_history_df)
     
-    best_account = sr_df.loc[sr_df['sr'].idxmax()]
-    
-    return best_account.to_dict()
+    best_account = sr_df.loc[sr_df['sr'].idxmax()].to_dict()
+
+    return best_account
 
 # @deprecated against using the full history of APY changes
 def calc_apy_last_year(df):
