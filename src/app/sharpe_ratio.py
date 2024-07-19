dd2573175034619529e106f0136690b2f32b56e7
Dez Oxel <dezoxel@gmail.com>
2024-07-19 18:54:01 -0400

    feat(sharpe_ratio): Risk rate added to report

--- src/app/sharpe_ratio.py
+++ src/app/sharpe_ratio.py
@@ -2,9 +2,10 @@ import pandas as pd
 from datetime import datetime
 from sqlite3 import Connection
 
+from domain.balance import convert_apy_to_annual_rate_terms_history
 from infra.risk_free_rate_repo import fetch_rfr_history_last_year
 from infra.savings_account_apy_history_repo import fetch_apy_history_last_year, fetch_terms_history_last_year
-from domain.account_sharpe_ratio import calc_apy_last_year, calc_best_savings_account_by_sharpe_ratio, calc_sharpe_ratio_for_all_accounts
+from domain.account_sharpe_ratio import calc_apy_last_year, calc_best_savings_account_by_sharpe_ratio, calc_compound_future_value_for_every_account, calc_return_rate_for_every_account, calc_sharpe_ratio_for_every_account, calc_std_returns_for_every_account
 from infra.savings_account_apy_last_year_repo import clear_apy_last_year
 from infra.savings_account_repo import find_savings_account_by_id
 
@@ -17,18 +18,6 @@ def update_apy_last_year(conn: Connection, current_date: datetime):
     df.to_sql('savings_accounts_apy_last_year', conn, if_exists='append', index=False)
     conn.commit()
 
-def find_best_savings_account_by_sharpe_ratio(conn: Connection, current_date: datetime, P: float) -> dict:
-    terms_history_df = fetch_terms_history_last_year(conn, current_date)
-    rfr_history_df = fetch_rfr_history_last_year(conn, current_date)
-
-    return calc_best_savings_account_by_sharpe_ratio(P, current_date, terms_history_df, rfr_history_df)
-
-def find_sharpe_ratio_for_all_accounts(conn: Connection, current_date: datetime, P: float) -> pd.DataFrame:
-    terms_history_df = fetch_terms_history_last_year(conn, current_date)
-    rfr_history_df = fetch_rfr_history_last_year(conn, current_date)
-
-    return calc_sharpe_ratio_for_all_accounts(P, current_date, terms_history_df, rfr_history_df)
-
 def print_best_savings_account_by_sharpe_ratio(best_account: dict):
     print(
         f"Best Savings Account by Sharpe Ratio is:\n"
@@ -38,11 +27,32 @@ def print_best_savings_account_by_sharpe_ratio(best_account: dict):
         f"Account ID: {best_account['account_id']:.0f}\n"
         f"Sharpe Ratio: {best_account['sr']:.4f}\n"
         f"ROI: {best_account['return_rate']*100:.2f}%\n"
+        f"Risk: {best_account['risk_place']} place among {best_account['total_accounts']} accounts (less is better)\n"
     )
 
-def find_and_print_best_savings_account_by_sharpe_ratio(conn: Connection, current_date: datetime, P: float):
-    best_account_stats = find_best_savings_account_by_sharpe_ratio(conn, current_date, P)
+def calc_risk_place(df, account_id):
+    risk_place = df.sort_values(by='std_returns')['account_id'].tolist().index(account_id) + 1
+    return risk_place
+
+def find_and_print_best_savings_account_by_sharpe_ratio_last_year(conn: Connection, current_date: datetime, P: float):
+    apy_terms_history = fetch_terms_history_last_year(conn, current_date)
+    rfr_history = fetch_rfr_history_last_year(conn, current_date)
+
+    annual_rate_terms_history = convert_apy_to_annual_rate_terms_history(apy_terms_history)
+
+    future_value_by_account = calc_compound_future_value_for_every_account(P, current_date, annual_rate_terms_history)
+    sharpe_ratio_df = calc_sharpe_ratio_for_every_account(future_value_by_account, rfr_history)
+    std_returns_df = calc_std_returns_for_every_account(future_value_by_account)
+    return_rate_df = calc_return_rate_for_every_account(future_value_by_account)
+    finance_info = sharpe_ratio_df.merge(std_returns_df, on='account_id').merge(return_rate_df, on='account_id')
+
+    best_account_stats = calc_best_savings_account_by_sharpe_ratio(finance_info)
+    risk_place = calc_risk_place(finance_info, best_account_stats['account_id'])
+
     account_details = find_savings_account_by_id(conn, best_account_stats['account_id'])
-    best_account = best_account_stats | account_details
-    print_best_savings_account_by_sharpe_ratio(best_account)
+
+    total_accounts = annual_rate_terms_history['account_id'].nunique()
+    best_account_report = best_account_stats | account_details | {'total_accounts': total_accounts, 'risk_place': risk_place}
+
+    print_best_savings_account_by_sharpe_ratio(best_account_report)
 
