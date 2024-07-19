4bdd3fff386ac46307793b3c4751ba491d06a4d9
Dez Oxel <dezoxel@gmail.com>
2024-07-19 14:47:06 -0400

    domain: calc_compound_future_value_with_dynamic_terms: 1) fix return rate history calculation, 2) add support for total return rate

--- src/domain/balance.py
+++ src/domain/balance.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime
 from typing import Dict, Tuple, List, Union
 
-from domain.compound_interest_math import calc_annual_rate_by_APY, calc_compound_future_value
+from domain.compound_interest_math import calc_annual_rate_by_APY, calc_compound_future_value, calc_return_rate
 from domain.datetime import calculate_time_diff_in_years
 
 Terms = Dict[str, Union[float, int, datetime]]
@@ -16,6 +16,7 @@ def get_terms_period(current_terms: Terms, next_terms: Terms | None, current_dat
     return terms_change_date, next_terms_change_date
 
 def calc_compound_future_value_with_dynamic_terms(P: float, terms_history: TermsHistory, current_date: datetime) -> FutureValueDetails:
+    accumulated_amount_prev_period = P
     accumulated_amount = P
     balance_history = [P]
     return_rate_history = [0]
@@ -26,23 +27,27 @@ def calc_compound_future_value_with_dynamic_terms(P: float, terms_history: Terms
         terms_change_date, next_terms_change_date = get_terms_period(terms_history[i], terms_history.get(i + 1, None), current_date)
         time_diff_years = calculate_time_diff_in_years(terms_change_date, next_terms_change_date)
 
+        accumulated_amount_prev_period = accumulated_amount
         accumulated_amount = calc_compound_future_value(accumulated_amount, interest_rate, compounding_frequency, time_diff_years)
-        return_rate = (accumulated_amount - P) / P
+        return_rate = calc_return_rate(accumulated_amount_prev_period, accumulated_amount)
 
         balance_history.append(accumulated_amount)
         return_rate_history.append(return_rate)
+
+    return_rate_total = calc_return_rate(P, accumulated_amount)
     
-    return accumulated_amount, balance_history, return_rate_history
+    return accumulated_amount, balance_history, return_rate_history, return_rate_total
 
-def calc_returns(P: float, current_date: datetime, account_terms_history: pd.Series) -> List[float]:
+def calc_future_value_stats(P: float, current_date: datetime, account_terms_history: pd.Series):
     annual_rate_terms_history = convert_apy_to_annual_rate_terms_history(account_terms_history)
-    _, _, return_rate_history = calc_compound_future_value_with_dynamic_terms(P, annual_rate_terms_history, current_date)
-    return return_rate_history[1:]
+    stats = calc_compound_future_value_with_dynamic_terms(P, annual_rate_terms_history, current_date)
+    return stats
 
-def calc_balance(P: float, current_date: datetime, account_terms_history: pd.Series) -> List[float]:
+def calc_balance(P: float, current_date: datetime, account_terms_history: pd.Series) -> float:
     annual_rate_terms_history = convert_apy_to_annual_rate_terms_history(account_terms_history)
     balance, _, _ = calc_compound_future_value_with_dynamic_terms(P, annual_rate_terms_history, current_date)
     return balance
+
 
 def convert_apy_to_annual_rate_terms_history(account_terms_history: pd.Series) -> pd.Series:
     terms = pd.Series(account_terms_history.apply(lambda t: {'APY': t['apy'], 'n': t['n'], 'date': t['date']}, axis=1)).reset_index(drop=True)
--- src/domain/compound_interest_math.py
+++ src/domain/compound_interest_math.py
@@ -5,3 +5,7 @@ def calc_compound_future_value(P: float, r: float, n: int, t: float) -> float:
 def calc_annual_rate_by_APY(n: int, APY: float) -> float:
     r_a = n * ((1 + APY)**(1 / n) - 1)
     return r_a
+
+def calc_return_rate(value_begin: float, value_end: float) -> float:
+    return_rate = (value_end - value_begin) / value_begin
+    return return_rate
