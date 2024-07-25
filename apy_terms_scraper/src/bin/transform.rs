bea43fe8f8564ac8d3c48938103749a4dbc8f69a
Dez Oxel <dezoxel@gmail.com>
2024-07-25 11:42:51 -0400

    feat(apy_terms_scraper): Error messages for Transform ELT step

--- apy_terms_scraper/src/bin/transform.rs
+++ apy_terms_scraper/src/bin/transform.rs
@@ -1,12 +1,12 @@
-use log::info;
+use async_openai::Client;
 use env_logger;
+use log::{error, info};
 use rusqlite::{Connection, Result};
 use std::error::Error;
-use async_openai::Client;
 
 use apy_terms_scraper::config::Config;
-use apy_terms_scraper::savings_account::SavingsAccountRepo;
 use apy_terms_scraper::db::write_apy_to_db;
+use apy_terms_scraper::savings_account::SavingsAccountRepo;
 use apy_terms_scraper::vec_comma_str::vec_to_comma_str;
 
 #[tokio::main]
@@ -25,9 +25,25 @@ async fn main() -> Result<(), Box<dyn Error>> {
     );
 
     for account in accounts {
-        let html = account.read_terms_html_from_file(&config.apy_html_path)?;
-        let terms_text = account.extract_terms_text_from_html(&html)?;
-        let apy = account.extract_apy_from_terms_text(&client, &terms_text).await?;
+        let html = account
+            .read_terms_html_from_file(&config.apy_html_path)
+            .map_err(|e| {
+                error!("Failed to read terms HTML from file for account {}: {}", account.id(), e);
+                e
+            })?;
+        let terms_text = account
+            .extract_terms_text_from_html(&html)
+            .map_err(|e| {
+                error!("Failed to extract terms text from HTML for account {}: {}", account.id(), e);
+                e
+            })?;
+        let apy = account
+            .extract_apy_from_terms_text(&client, &terms_text)
+            .await
+            .map_err(|e| {
+                error!("Failed to extract APY from terms text for account {}: {}", account.id(), e);
+                e
+            })?;
         write_apy_to_db(&conn, account.id(), apy)?;
     }
 
